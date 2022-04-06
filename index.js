94ad0f7ff0e04da3b61fac87037992abdc2faeee
Patrick Hart <pathart612@gmail.com>
2022-04-06 13:43:30 -0400

    commit tree 410af1088f79b2ddf28f4ecb4b8de4aa8bbc8c63

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers, person){
+    return drivers.filter(drivers => drivers.toLowerCase() === person.toLowerCase());
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
